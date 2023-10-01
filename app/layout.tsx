9e43dec555db6a97adeb0b189f5398a08baafe58
jpabl@0s1n <jpablo_166@hotmail.com>
2023-09-30 22:38:54 -0500

    fix(core): improve metada, change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'JS Mastery',
   description: 'JS Mastery Resources',
+  other: {
+    'theme-color': '#0d1117',
+    'color-scheme': 'dark only',
+    'twitter:image': 'https://i.ibb.co/d6TXxB2/homepage-thumnail.jpg',
+    'twitter:card': 'summary_large_image',
+    'og:url': 'jsmastery.pro',
+    'og:image': 'https://i.ibb.co/d6TXxB2/homepage-thumnail.jpg',
+    'og:type': 'website',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
